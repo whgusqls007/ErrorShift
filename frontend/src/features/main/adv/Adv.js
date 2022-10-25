6f1c8cb9c523c3d38ac5bc60500700f16e173f1f
유도경 <dbehrud9323@hanmail.net>
2022-10-26 08:58:10 +0900

    modify navbar

---
+++ frontend/src/features/main/adv/Adv.js
@@ -0,0 +1,10 @@
+import NavBar from "../../../common/navbar/NavBar"
+
+function Adv(){
+    return(
+        <div>
+            <NavBar></NavBar>
+        </div>
+    )
+}
+export default Adv;
---
+++ frontend/src/features/main/errortype/ErrorType.js
@@ -0,0 +1,10 @@
+import NavBar from "../../../common/navbar/NavBar"
+
+function ErrorType(){
+    return(
+        <div>
+            <NavBar></NavBar>
+        </div>
+    )
+}
+export default ErrorType;
