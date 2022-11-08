6950134de85ef87a5c9fad303d55320d43ff300d
유도경 <dbehrud9323@hanmail.net>
2022-11-08 14:40:17 +0900

    [Refactor] Code refactoring

--- frontend/src/common/sidebar/SideBar.js
+++ frontend/src/common/sidebar/SideBar.js
@@ -1,4 +1,4 @@
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import { Link } from 'react-router-dom';
 
 function SideBar(){
