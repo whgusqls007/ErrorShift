7a28f1f20acdf4154c479ae609e24485c6176ebc
김유리 <goldflowed@gmail.com>
2022-11-09 16:44:33 +0900

    [feat] fix download url part

--- fileServer/index.js
+++ fileServer/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.get("/filehome", (req, res) => {
+  res.send("/filehome requested")
+})
+
 app.get("/file/download", (req, res) => {
   console.log('index.js /file/download get called')
   res.download("./e206-1.0.0.jar");
