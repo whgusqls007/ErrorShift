4ba81ecf83be98ea974315a5a8a97f0d671dc268
김유리 <goldflowed@gmail.com>
2022-11-09 16:37:13 +0900

    [feat] fix download url part

--- fileServer/index.js
+++ fileServer/index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3001;
 
 app.get("/file/download", (req, res) => {
+  console.log('index.js /file/download get called')
   res.download("./e206-1.0.0.jar");
 });
 
