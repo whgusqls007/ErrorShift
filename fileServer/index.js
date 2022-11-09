aa4155834ea016ddee4e43ec3b37a62afeb4568b
김유리 <goldflowed@gmail.com>
2022-11-09 16:29:30 +0900

    [feat] fix dockerfile

--- fileServer/index.js
+++ fileServer/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/download", (req, res) => {
+app.get("/file/download", (req, res) => {
   res.download("./e206-1.0.0.jar");
 });
 
