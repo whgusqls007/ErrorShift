233856552c9ad168155769e5c9322e085609bafe
조현빈 <jo7hb@naver.com>
2022-11-18 17:46:19 +0900

    [fix] delete dependencies

--- test/src/main/java/com/example/test/exceptionHandler/GlobalExceptionHandler.java
+++ test/src/main/java/com/example/test/exceptionHandler/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.example.test.exceptionHandler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.ssafy.e206.annotation.ErrorShift;
 
@@ -9,4 +13,10 @@ import com.ssafy.e206.annotation.ErrorShift;
 @ErrorShift(exception = ArithmeticException.class, message = "Arithmetic Exception", httpStatus = HttpStatus.NOT_FOUND, language = "ko")
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+  @ExceptionHandler(NoHandlerFoundException.class)
+  public ResponseEntity<String> handle404(NoHandlerFoundException e) {
+    return new ResponseEntity<String>("404", HttpStatus.NOT_FOUND);
+  }
+
 }
